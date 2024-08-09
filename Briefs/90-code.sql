9611eace1a25d0fb48356125ec3b510c2d0ebd96
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-09 23:39:12 +0300

    task 11

--- Briefs/90-code.sql
+++ Briefs/90-code.sql
@@ -67,3 +67,16 @@ CREATE INDEX idx_name_first ON names (name(1))
 CREATE INDEX idx_name_first_score
 ON names(name(1), score);
 
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
+BEGIN
+	RETURN (IF (b = 0, 0, a / b));
+END
+"""
+    script that creates a function SafeDiv
+    that divides (and returns) the first
+    by the second number or returns 0 if
+    the second number is equal to 0.
+"""
+
