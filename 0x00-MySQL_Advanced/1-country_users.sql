2ea92673bb2c0afba6170e55e19942a1c83cfba0
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-09 04:02:14 +0300

    fixing typo error in email

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -2,7 +2,7 @@
 -- enumeration of countries added
 CREATE table IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    emial VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 )
