bb2d813caf65bdffb0e34298bc4dda6eb5a4007d
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-13 12:45:49 +0300

    trial

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -40,18 +40,44 @@ from pymongo import MongoClient
 #     print("\tmethod PATCH: {}".format(patch))
 #     print("\tmethod DELETE: {}".format(delete))
 #     print("{} status check".format(get_and_status))
-def main():
-    """ main func
+
+# def main():
+#     """ main func
+#     """
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     collection = client.logs.nginx
+#     total = collection.count_documents({})
+#     get = collection.count_documents({"method": "GET"})
+#     post = collection.count_documents({"method": "POST"})
+#     put = collection.count_documents({"method": "PUT"})
+#     patch = collection.count_documents({"method": "PATCH"})
+#     delete = collection.count_documents({"method": "DELETE"})
+#     path = collection.count_documents(
+#         {"method": "GET", "path": "/status"})
+#     print(f"{total} logs")
+#     print("Methods:")
+#     print(f"\tmethod GET: {get}")
+#     print(f"\tmethod POST: {post}")
+#     print(f"\tmethod PUT: {put}")
+#     print(f"\tmethod PATCH: {patch}")
+#     print(f"\tmethod DELETE: {delete}")
+#     print(f"{path} status check")
+
+
+# if __name__ == "__main__":
+#     main()
+def log_stats():
+    """ log_stats.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
-    total = collection.count_documents({})
-    get = collection.count_documents({"method": "GET"})
-    post = collection.count_documents({"method": "POST"})
-    put = collection.count_documents({"method": "PUT"})
-    patch = collection.count_documents({"method": "PATCH"})
-    delete = collection.count_documents({"method": "DELETE"})
-    path = collection.count_documents(
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
@@ -64,4 +90,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    log_stats()
