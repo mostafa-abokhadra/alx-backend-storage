347f671c4cbc738848e4afd530f60024ead4d513
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-13 08:33:12 +0300

    task 11 fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,5 +8,5 @@ def update_topics(mongo_collection, name, topics):
     """
     mongo_collection.update_many(
         {"name": name},
-        {"$set", {"topics": topics}}
+        {"$set": {"topics": topics}}
     )
