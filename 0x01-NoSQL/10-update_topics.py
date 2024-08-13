36d6d643c74bc478f160319e3e0db22bcc1f3909
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-13 08:30:34 +0300

    task 11

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""pymongo practice
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changing all school topics based on name
+    """
+    mongo_collection.update_many(
+        {name: name},
+        {"$set", {topics: topics}}
+    )
