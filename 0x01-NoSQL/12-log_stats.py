5516d6cb0d65f3a0c178a05c2f5273cc437f1b59
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-13 12:39:11 +0300

    count_document() replace

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,42 +4,69 @@
 from pymongo import MongoClient
 
 
+# def main():
+#     """main function
+#     """
+#     client = MongoClient('mongodb://127.0.0.1:27017')
+#     db = client['logs']
+#     collection = db.nginx
+#     collection.create_index('method')
+#     data = collection.find()
+
+#     listy = list(data)
+#     lenght = len(listy)
+#     get, post, put, patch, delete = 0, 0, 0, 0, 0
+#     get_and_status = 0
+
+#     for dict in listy:
+#         if dict['method'] == 'GET':
+#             get = get + 1
+#         elif dict['method'] == 'POST':
+#             post = post + 1
+#         elif dict['method'] == 'PATCH':
+#             patch += 1
+#         elif dict['method'] == 'PUT':
+#             put = put + 1
+#         elif dict['method'] == 'DELETE':
+#             delete = delete + 1
+#         if dict['method'] == 'GET' and dict['path'] == '/status':
+#             get_and_status = get_and_status + 1
+
+#     print(lenght, "logs")
+#     print("Methods:")
+#     print("\tmethod GET: {}".format(get))
+#     print("\tmethod POST: {}".format(post))
+#     print("\tmethod PUT: {}".format(put))
+#     print("\tmethod PATCH: {}".format(patch))
+#     print("\tmethod DELETE: {}".format(delete))
+#     print("{} status check".format(get_and_status))
+
+
+# if __name__ == "__main__":
+#     main()
+
+
 def main():
-    """main function
+    """ main func
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client['logs']
-    collection = db.nginx
-    collection.create_index('method')
-    data = collection.find()
-
-    listy = list(data)
-    lenght = len(listy)
-    get, post, put, patch, delete = 0, 0, 0, 0, 0
-    get_and_status = 0
-
-    for dict in listy:
-        if dict['method'] == 'GET':
-            get = get + 1
-        elif dict['method'] == 'POST':
-            post = post + 1
-        elif dict['method'] == 'PATCH':
-            patch += 1
-        elif dict['method'] == 'PUT':
-            put = put + 1
-        elif dict['method'] == 'DELETE':
-            delete = delete + 1
-        if dict['method'] == 'GET' and dict['path'] == '/status':
-            get_and_status = get_and_status + 1
-
-    print(lenght, "logs")
+    collection = client.logs.nginx
+    total = collection.count_documents({})
+    get = collection.count_documents({"method": "GET"})
+    post = collection.count_documents({"method": "POST"})
+    put = collection.count_documents({"method": "PUT"})
+    patch = collection.count_documents({"method": "PATCH"})
+    delete = collection.count_documents({"method": "DELETE"})
+    path = collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
     print("Methods:")
-    print("    method GET: {}".format(get))
-    print("    method POST: {}".format(post))
-    print("    method PUT: {}".format(put))
-    print("    method PATCH: {}".format(patch))
-    print("    method DELETE: {}".format(delete))
-    print("{} status check".format(get_and_status))
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
 
 
 if __name__ == "__main__":
