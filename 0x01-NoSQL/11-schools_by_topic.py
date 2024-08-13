bd8d4de7b7e87bbb469daa195e67428e717db4d7
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-13 08:47:19 +0300

    task 12

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,9 +6,10 @@
 def schools_by_topic(mongo_collection, topic):
     """list of school with certain topic
     """
-    contain = []
-    all = mongo_collection.find()
-    for dictionary in all:
-        if topic in dictionary.topics:
-            contain.append(dict(dictionary))
-    return contain
+    # contain = []
+    # all = mongo_collection.find()
+    # for dictionary in all:
+    #     if topic in dictionary.topics:
+    #         contain.append(dict(dictionary))
+    # return contain
+     return mongo_collection.find({"topics": topic})
