7509da4107490c2c999ad56f9052141d9a601602
mostafa-abokhadra <mvxizk@gmail.com>
2024-08-15 05:08:09 +0300

    styling fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -49,6 +49,7 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+
 def count_calls(method: Callable) -> Callable:
     """ # of calls made to Cashe class methods
     """
@@ -81,20 +82,19 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-        """getting and converting byte to desired
-        """ 
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float]:
+        """getting and converting byte to desired"""
         data = self._redis.get(key)
         return fn(data) if fn is not None else data
-    
+
     def get_str(self, key: str) -> str:
         """get string
         """
         return self.get(key, lambda x: x.decode('utf-8'))
-    
+
     def get_int(self, key: str) -> int:
         """get integer
         """
         return self.get(key, lambda x: int(x))
-
